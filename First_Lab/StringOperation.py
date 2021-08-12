a0b80d92a7be2ce54dac6b8578bb961907dfa775
Siddhant <siddhantsharma909@gmail.com>
2021-08-12 11:00:35 +0530

    Initial Commit

---
+++ First_Lab/StringOperation.py
@@ -0,0 +1,16 @@
+
+firstName = "Siddhant"
+lastName = "Sharma"
+
+fullName = firstName + " " + lastName
+print(fullName)
+
+msg = f"{fullName} is a coder"
+print(msg)
+
+print(len(fullName))
+print(fullName.upper())
+print(fullName.lower())
+print(fullName.find('a'))
+print(fullName.replace("Sharma", "Kumar Sharma"))
+print("Sharma" in fullName)
